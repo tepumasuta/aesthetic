80a9cd01ff028ac741f49a97f07d776655c33c88
tepumasuta <thinkingocean2@gmail.com>
2022-06-13 23:13:47 +0300

    Added new token types `Value`, `Operator`, `Keyword` & corresponding types: `value_type`, `operation_type`, `keyword_type`. Added Converting tables for pretty token printing.

--- src/token.c
+++ src/token.c
@@ -3,9 +3,30 @@
 
 #include "assert.h"
 
-static_assert(TOKEN_TYPE_SIZE == 1, "Not all token_type values are handled");
-static const char* TOKEN_TYPE_CONVERT_TABLE[] = {"Operation"};
+static_assert(TOKEN_TYPE_SIZE == 3, "Not all token_type values are handled");
+static const char* TOKEN_TYPE_CONVERT_TABLE[] = {"Operator", "Value", "Keyword"};
+static_assert(OPERATION_TYPE_SIZE == 5, "Not all operation_type values are handled");
+static const char* OPERATION_TYPE_CONVERT_TABLE[] = {"+", "-", "*", "/", "//"};
+static_assert(VALUE_TYPE_SIZE == 1, "Not all value_type values are handled");
+static const char* VALUE_TYPE_CONVERT_TABLE[] = {"int"};
+static_assert(KEYWORDS_SIZE == 1, "Not all keyword_type values are handled");
+static const char* KEYWORD_TYPE_CONVERT_TABLE[] = {"if"};
 
 void print_token(token t) {
-    printf("Token{%s} `%s`\n", TOKEN_TYPE_CONVERT_TABLE[t.type], t.contents);
+    printf("Token{%s}", TOKEN_TYPE_CONVERT_TABLE[t.type]);
+    switch (t.type) {
+        case Operator:
+            printf(" `%s`", OPERATION_TYPE_CONVERT_TABLE[t.op.op_type]);
+            break;
+        case Value:
+            printf(" %s: `%s`", VALUE_TYPE_CONVERT_TABLE[t.val.val_type], t.val.contents);
+            break;
+        case Keyword:
+            printf(" `%s`", KEYWORD_TYPE_CONVERT_TABLE[t.kw.kw_type]);
+            break;
+        case TOKEN_TYPE_SIZE:
+            assert(0 && "Wrong token type in `print_token`");
+            break;
+    }
+    putc('\n', stdout);
 }
--- src/token.h
+++ src/token.h
@@ -2,15 +2,50 @@
 #define TOKEN_H
 
 enum token_type {
-    Operation=0,
+    Operator=0,
+    Value=1,
+    Keyword=2,
     TOKEN_TYPE_SIZE
 };
 
+enum value_type {
+    Integer=0,
+    VALUE_TYPE_SIZE
+};
+
+enum operation_type {
+    Addition=0,
+    Substraction=1,
+    Multiplication=2,
+    FloatDivision=3,
+    IntegerDivision=4,
+    OPERATION_TYPE_SIZE
+};
+
+enum keyword_type {
+    If=0,
+    KEYWORDS_SIZE
+};
+
+typedef struct {
+    enum operation_type op_type;
+} operation_token;
+
+typedef struct {
+    const char *contents;
+    enum value_type val_type;
+} value_token;
+
+typedef struct {
+    enum keyword_type kw_type;
+} keyword_token;
+
 typedef struct {
     enum token_type type;
-    const char *contents;
     union {
-
+        operation_token op;
+        value_token val;
+        keyword_token kw;
     };
 } token;
 
