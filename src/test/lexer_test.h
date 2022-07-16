c3fe01f26976bc3092846b6dc8dfe2ccd9c1f887
tepumasuta <thinkingocean2@gmail.com>
2022-07-16 14:38:30 +0300

    Added basic headers for test & lexer_test

---
+++ src/test/lexer_test.h
@@ -0,0 +1,10 @@
+#ifndef LEXER_TEST_H
+#define LEXER_TEST_H
+
+#include "test.h"
+#include "stddef.h"
+
+void test_lexer_full();
+test_result test_lexer_unit(size_t test_index);
+
+#endif // LEXER_TEST_H
---
+++ src/test/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "stdbool.h"
+
+typedef struct test_result {
+    bool passed;
+} test_result;
+
+#endif // TEST_H
